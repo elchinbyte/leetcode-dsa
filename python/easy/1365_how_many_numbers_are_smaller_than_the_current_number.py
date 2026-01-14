acd357baf3eb34f29f45feb9fd98bcf3d537989e
Elchin Byte <elchinbyte@gmail.com>
2026-01-14 20:31:56 +0500

    feat: add easy array problem 1365 solution

---
+++ python/easy/1365_how_many_numbers_are_smaller_than_the_current_number.py
@@ -0,0 +1,18 @@
+# LeetCode 1365: How Many Numbers Are Smaller Than the Current Number
+# Difficulty: Easy
+# Topics: Arrays, Hash Table, Sorting, Counting sort
+# Language: Python 3
+
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        res = []
+        
+        for i in nums:
+            count = 0
+            n = i
+            for x in nums:
+                if x < n:
+                    count += 1
+            res.append(count)
+
+        return res
