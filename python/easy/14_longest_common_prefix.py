cdbb9ade6eb05f7db48ea4d93e16b34fbca6b6c0
Elchin Byte <elchinbyte@gmail.com>
2026-01-15 22:09:19 +0500

    feat: add easy array problem 14 solution

---
+++ python/easy/14_longest_common_prefix.py
@@ -0,0 +1,14 @@
+# LeetCode 14: Longest Common Prefix
+# Difficulty: Easy
+# Topics: Arrays, String, Trie
+# Language: Python 3
+
+from typing import List
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        for i in range(len(strs[0])):
+            for string in strs[1:]:
+                if len(string) <= i or string[i] != strs[0][i]:
+                    return strs[0][:i]
+        return strs[0]
